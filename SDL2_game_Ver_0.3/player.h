c5f75c1d54007e3c8533bdbfc05ac05e978cc7f3
gre4te4r <duy74547@gmail.com>
2025-03-27 03:28:27 +0700

    Add files via upload

---
+++ SDL2_game_Ver_0.3/player.h
@@ -0,0 +1,35 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+#include <string>
+#include <map>
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include "GameObject.h"
+#include"TextureManage.h"
+#include"InputHandler.h"
+
+class Player : public GameObject{
+
+public:
+    void load(int x, int y, int width, int height, std::string textureID);
+
+    void draw(SDL_Renderer* pRenderer);
+
+    void update();
+
+
+    Vector2D playerGetPosition();
+
+    SDL_RendererFlip getPlayerFlip();
+
+    int getPlayerWidth();
+
+    int getPlayerHeight();
+
+    void clean();
+
+};
+
+#endif // PLAYER_H_INCLUDED
